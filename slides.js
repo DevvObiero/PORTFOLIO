4b6dc04e8c0ccf92b1c95374312a71c331cf7d76
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2026-07-13 22:45:22 +0300

    highlighjting changed to beige and not blue

--- slides.js
+++ slides.js
@@ -3,25 +3,30 @@ export const slides = [
     title: "Blackwater '91",
     description: "Flickering lanterns and twisted masks welcome unwanted visitors into a strange celebration beyond the forest trail.",
     image: "./project1.png",
+    link: "p2.html",
   },
   {
     title: "Crimson Theory",
     description: "A mysterious performer slowly loses reality beneath violent lights and unsettling mirrored reflections inside an empty theater.",
     image: "./project2.png",
+    link: "p3.html",
   },
   {
     title: "Tape Delay Archives",
     description: "Stacks of dusty videotapes and glowing static fill the room during another endless night without a single moment of sleep.",
- image: "./project3.png",
+    image: "./project3.png",
+    link: "p9.html",
   },
   {
     title: "Exit 14 Westbound",
     description: "A lone traveler walks the empty highway at night, guided only by the flickering lights of passing cars and the distant glow of a city skyline.",
- image: "./project4.png",
+    image: "./project4.png",
+    link: "p8.html",
   },
-    {
+  {
     title: "Exit 14 Westbound",
     description: "A lone traveler walks the empty highway at night, guided only by the flickering lights of passing cars and the distant glow of a city skyline.",
- image: "./project5.png",
+    image: "./project5.png",
+    link: "p10.html",
   },
 ];
